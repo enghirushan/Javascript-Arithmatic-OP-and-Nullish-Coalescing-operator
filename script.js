41dbff8002702b3534c96b0bbdfa10083b20e00d
Ilangasinghe I <24967679-enghirushan@users.noreply.replit.com>
2024-02-24 05:23:35 +0000

    Add Nullish Coalescing operator

--- script.js
+++ script.js
@@ -23,3 +23,25 @@ console.log(g);
 const h = 0;
 const i = h || "Mars";
 console.log(i);  
+
+//when Nullish Coalescing operator
+const j = 0;
+const k = j ?? "Jupiter";
+console.log(k);
+
+
+const l = null;
+const m = l ?? "Saturn";
+console.log(m);
+
+const n = undefined;
+const p = n ?? "Uranus";
+console.log(p);
+
+
+//do not show the value of the variable
+const q = "";
+const r = q ?? "Neptune";
+console.log(q);
+
+//nullish coalescing mean if the value is null or undefined then it will show the value of the variable
